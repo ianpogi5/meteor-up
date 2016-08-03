b8176cc1746aa6325d1e202cb91bf703779b99ba
Ian dela Cruz <iandc76@gmail.com>
2016-08-03 21:35:05 +0800

    Fix for Ubuntu 14.04

--- scripts/linux/install-mongodb.sh
+++ scripts/linux/install-mongodb.sh
@@ -8,7 +8,7 @@ sudo dpkg --configure -a
 set -e
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update -y
 sudo apt-get install mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-tools -y
 
