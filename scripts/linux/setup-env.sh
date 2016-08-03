2c6f264e285eddb260174efbf7ba6b825c6dea25
Ian dela Cruz <iandc76@gmail.com>
2016-08-03 22:06:58 +0800

    fix faulty mongodb bson dependency

--- scripts/linux/setup-env.sh
+++ scripts/linux/setup-env.sh
@@ -10,5 +10,9 @@ sudo chown ${USER} /etc/
 
 sudo npm install -g forever userdown wait-for-mongo node-gyp
 
+# fix faulty mongodb bson dependency
+cd /opt/nodejs/lib/node_modules/wait-for-mongo/node_modules/mongodb/node_modules/bson/build/Release
+sudo ln -s ../../browser_build/bson.js .
+
 # Creating a non-privileged user
 sudo useradd meteoruser || :
